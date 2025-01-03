a8a18451f6ee866eb355cfa505457380781b0d53
AirIHL <inhak1122@gmail.com>
2025-01-03 16:47:59 +0900

    Initial implementation of calculator

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -6,20 +6,47 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("계산할 첫번째 숫자를 입력하세요!");
-        int a = scan.nextInt();
+        int result = 0;
+        boolean reVal = true;
 
-        System.out.print("계산할 두번째 숫자를 입력하세요!");
-        int b = scan.nextInt();
+        while(reVal) {
+            System.out.print("계산할 첫번째 숫자를 입력하세요! : ");
+            int a = scan.nextInt();
 
-        System.out.print("사칙연산 기호를 입력하세요!");
-        String c = scan.next();
-        char allSym = c.charAt(0);
+            System.out.print("계산할 두번째 숫자를 입력하세요! : ");
+            int b = scan.nextInt();
 
-        if(!(allSym == '*' || allSym == '/' || allSym == '+' || allSym == '-')) {
-            System.out.println("기호를 잘 못 입력했습니다!!! 다시 입력하세요!!!");
+            System.out.print("사칙연산 기호를 입력하세요! : ");
+            String c = scan.next();
+            char allSym = c.charAt(0);
+
+            switch (allSym) {
+
+                case '*':
+                    result = a * b;
+                    break;
+                case '/':
+                    result = a / b;
+                    break;
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                default:
+                    System.out.println("기호를 잘 못 입력했습니다!!! 다시 입력하세요!!!");
+                    break;
+            }
+
+            System.out.println("결과 : " + result);
+            System.out.println();
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료) : ");
+            String rework = scan.next();
+
+            if(rework.equals("exit")) {
+                reVal = false;
+            }
         }
-
-
     }
 }
