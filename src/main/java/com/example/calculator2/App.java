68a765b57bbd1ac1bfeb5fd10b440c201857afb2
AirIHL <inhak1122@gmail.com>
2025-01-10 13:28:07 +0900

    feat(calculator2): modify input sequence and add documentation
    - Change input order in Calculator class
    - Add README.txt file
    - Add code comments

--- src/main/java/com/example/calculator2/App.java
+++ src/main/java/com/example/calculator2/App.java
@@ -7,29 +7,32 @@ public class App {
     public static void main(String[] args) {
 
 
-
         Scanner scan = new Scanner(System.in);
         Calculator cal = new Calculator();
-        boolean reVal = true;
+        boolean reVal = true;    // while 문에 들어가 작업을 계속할지를 받는 boolean, 초기값을 true로 지정하고 'exit' 이 들어오면 false로 바뀐다.
         int result = 0;
 
-        while(reVal) {
+        while (reVal) {
+            // 사용자로 부터 값을 입력받는 곳
             System.out.print("계산할 첫번째 숫자를 입력하세요! : ");
             int a = scan.nextInt();
+
+            System.out.print("사칙연산 기호를 입력하세요! : ");
+            String c = scan.next();
+            char allSym = c.charAt(0);  // String으로 받은 값 중 첫번째 값만 char 형식으로 allSym에 저장
 
             System.out.print("계산할 두번째 숫자를 입력하세요! : ");
             int b = scan.nextInt();
 
-            System.out.print("사칙연산 기호를 입력하세요! : ");
-            String c = scan.next();
-            char allSym = c.charAt(0);
-
             cal.Calculate(a, b, allSym);
+            // Getter를 통해 값을 받아옴
             LinkedList<Integer> results = cal.getResultData();
 
-            if(results.size() > 6)
+            // results의 값이 6개가 넘어가면 Setter 메서드를 통해 값이 삭제
+            if (results.size() > 6)
                 cal.setRemoveFirstResult();
 
+            // 결과값 출력 및 더 계산할지 여부 입력 받음
             System.out.println("결과 : " + results.getLast());
             System.out.println("========================");
             System.out.println("전체 계산 기록: " + results);
@@ -38,7 +41,8 @@ public class App {
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료) : ");
             String rework = scan.next();
 
-            if(rework.equals("exit")) {
+            // 입력 받은 rework가 'exit'과 일치하는지 확인 후 일치하면 reVal을 false로 변환
+            if (rework.equals("exit")) {
                 reVal = false;
             }
         }
--- src/main/java/com/example/calculator2/Calculator.java
+++ src/main/java/com/example/calculator2/Calculator.java
@@ -9,18 +9,22 @@ public class Calculator {
     int b; // 값 2
     char sym; // 연산 기호
 
+    // private로  연산 결과를 저장하고 있는 컬렉션 필드에 직접 접근하지 못하도록 수정
     private LinkedList<Integer> resultData = new LinkedList<Integer>();
 
+    // Getter로 값 반환
     public LinkedList<Integer> getResultData() {
         return resultData;
     }
 
+    // Setter로 값 수정, resultData에 값이 있을때 수행
     public void setRemoveFirstResult() {
-                LinkedList<Integer> newList = new LinkedList<>(resultData);
-                newList.remove(0);
-                this.resultData = newList;
+        if (!resultData.isEmpty()) {
+            resultData.removeFirst();
+        }
     }
 
+    // 계산하는 메서드
     public void Calculate(int valA, int valB, char allSym) {
         a = valA;
         b = valB;
